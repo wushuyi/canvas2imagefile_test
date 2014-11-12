b440eed429215203a3f55b5612912b67cc32df3f
巫书轶 <wushuyi541@gmail.com>
2014-11-12 14:41:33 +0800

    init

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,13 +1,16 @@
 (function($){
 	$(document).ready(function (){
 
+		// 工具集合
 		var util = {
+			// 将 blob 生成 url 链接
 			stream2Url: function(stream){
 				var objectUrl =  window.URL ? window.URL.createObjectURL(stream) : stream;
 				return objectUrl;
 			}
 		};
 
+		// 初始化环境变量
 		var imgSrc = './assets/images/beastie.png';
 		var img = new Image();
 		var canvas = document.createElement('canvas');
@@ -15,7 +18,10 @@
 		img.addEventListener('load', imgLoad);
 		img.src = imgSrc;
 
+		// 加载图片完成后运行
 		function imgLoad(e){
+
+			// 将图片装到canvas 中并且导出 base64
 			var img = this;
 			canvas.width = img.width;
 			canvas.height = img.height;
@@ -24,13 +30,17 @@
 			var base64Img = canvas.toDataURL();
 			//document.write(base64Img);
 			//console.log(base64Img);
+
+			// 解析 base64
 			base64Img = base64Img.split(';base64,')[1];
 			//console.log(base64Img);
 			base64Img = base64.decode(base64Img);
 			//console.log(base64Img);
 
-
+			// 将 转码后的图片 装入图片对象
 			var generatedFile = new File([base64Img], "test.png", {type: "image/png", lastModified: new Date()});
+
+			// 读取改图片对象
 			var reader = new FileReader();
 			reader.readAsText(generatedFile);
 			reader.addEventListener('load', function(e){
@@ -40,23 +50,24 @@
 			//console.log(util.stream2Url(generatedFile));
 		}
 
+		// input 读取文件
 		$('input[name="file"]').on('change', function(e){
 			var files = e.target.files;
 			if(files.length > 0){
 				for(var i=0, j = files.length; i < j; i++){
 					console.log(files[i]);
 					//console.log(util.stream2Url(files[i]));
+					// 解析文件内容
 					var reader = new FileReader();
 					reader.readAsText(files[i]);
 					reader.addEventListener('load', function(e){
+						// 在控制台输出 内容
 						console.log(typeof  this.result);
 					});
 				}
 			}
 			var $self = $(this);
 			$self.val('');
-
 		});
-
 	});
 })(window.jQuery);
